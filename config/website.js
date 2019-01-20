6a02e37436cc021156b0e1416c6980945037250e
Will <will.js@protonmail.com>
2019-01-20 07:32:16 -0600

    Update configs and add gitignore

--- config/website.js
+++ config/website.js
@@ -1,10 +1,10 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: 'Gatsby Starter - Prismic.io', // Navigation and Site Title
-  titleAlt: 'Gatsby Prismic.io', // Title for JSONLD
-  description: 'A typography-heavy & light-themed Gatsby Starter which uses the Headless CMS Prismic.',
-  headline: 'Writing and publishing content for LekoArts', // Headline for schema.org JSONLD
-  url: 'https://prismic.lekoarts.de', // Domain of your site. No trailing slash!
+  title: 'Billys Blog', // Navigation and Site Title
+  titleAlt: 'Billys Blog', // Title for JSONLD
+  description: 'Random Billy thoughts by Billy',
+  headline: 'Random Billy thoughts by Billy', // Headline for schema.org JSONLD
+  url: 'https://sugarobjects.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   logo: '/logos/logo-1024.png', // Used for SEO
   ogLanguage: 'en_US', // Facebook Language
